8b320e3039977e8af5898d8804dce59a73baf739
Nicolas Maison <ngaly@hotmail.fr>
2020-08-14 15:58:20 +0200

    rollback namespace

--- WebApplication/SiteMVC/Repositories/Repository.cs
+++ WebApplication/SiteMVC/Repositories/Repository.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SiteMVC.DataAccess;
 
 namespace SiteMVC.Repositories {
     public class Repository<T> : IRepository<T> where T : class {
--- WebApplication/SiteMVC/Startup.cs
+++ WebApplication/SiteMVC/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SiteMVC.Repositories;
-using SiteMVC.DataAccess;
 
 namespace SiteMVC {
     public class Startup {
