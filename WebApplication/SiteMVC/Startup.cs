ffd64a6b8fafbc18603eda7427f39054364de1cd
Nicolas Maison <ngaly@hotmail.fr>
2020-08-14 15:01:16 +0200

    namespace DataAccess + usings

--- WebApplication/SiteMVC/Startup.cs
+++ WebApplication/SiteMVC/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SiteMVC.Repositories;
+using SiteMVC.DataAccess;
 
 namespace SiteMVC {
     public class Startup {
