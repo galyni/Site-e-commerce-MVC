45305785d6d9a913c6b45b077bcf9f00e3f7a620
Nicolas Maison <ngaly@hotmail.fr>
2020-08-18 10:53:40 +0200

    structure (controller + modele) pour le login

---
+++ WebApplication/SiteMVC/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SiteMVC.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
--- WebApplication/SiteMVC/Startup.cs
+++ WebApplication/SiteMVC/Startup.cs
@@ -42,7 +42,9 @@ namespace SiteMVC {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseRequestLocalization("fr-FR", "en-UY");
+
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions {
