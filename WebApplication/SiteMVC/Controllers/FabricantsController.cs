b11c325e8186323697829158338455dfe415ef85
Nicolas Maison <ngaly@hotmail.fr>
2020-08-19 18:16:57 +0200

    Fabricant et Fournisseurs controllers

---
+++ WebApplication/SiteMVC/Controllers/FabricantsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SiteMVC.Repositories;
+
+namespace SiteMVC.Controllers {
+    public class FabricantsController : Controller {
+        IRepository<Fabricant> _repository;
+        public FabricantsController(IRepository<Fabricant> repository) {
+            _repository = repository;
+        }
+        // GET: Fabricants
+        public ActionResult Index() {
+            var liste = _repository.GetList();
+            return View(liste);
+        }
+
+        // GET: Fabricants/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Fabricants/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricant fabricant) {
+            try {
+                _repository.Create(fabricant);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Fabricants/Edit/5
+        public ActionResult Edit(int id) {
+            Fabricant Fabricant = _repository.GetById(id);
+            return View(Fabricant);
+        }
+
+        // POST: Fabricants/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricant fabricant) {
+            try {
+                _repository.Update(fabricant);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Fabricants/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id) {
+            var item = _repository.GetById(id);
+            return View(item);
+        }
+
+        // POST: Fabricants/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Fabricant fabricant) {
+            try {
+                _repository.Delete(fabricant);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication/SiteMVC/Controllers/FournisseursController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SiteMVC.Repositories;
+
+namespace SiteMVC.Controllers {
+    public class FournisseursController : Controller {
+        IRepository<Fournisseur> _repository;
+        public FournisseursController(IRepository<Fournisseur> repository) {
+            _repository = repository;
+        }
+        // GET: Fournisseurs
+        public ActionResult Index() {
+            var liste = _repository.GetList();
+            return View(liste);
+        }
+
+        // GET: Fournisseurs/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Fournisseurs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fournisseur fournisseur) {
+            try {
+                _repository.Create(fournisseur);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Fournisseurs/Edit/5
+        public ActionResult Edit(int id) {
+            Fournisseur Fournisseur = _repository.GetById(id);
+            return View(Fournisseur);
+        }
+
+        // POST: Fournisseurs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fournisseur fournisseur) {
+            try {
+                _repository.Update(fournisseur);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Fournisseurs/Delete/5
+        public ActionResult Delete(int id) {
+            var item = _repository.GetById(id);
+            return View(item);
+        }
+
+        // POST: Fournisseurs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Fournisseur fournisseur) {
+            try {
+                _repository.Delete(fournisseur);
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
